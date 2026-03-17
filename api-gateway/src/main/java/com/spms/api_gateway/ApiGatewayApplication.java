d45144d0817d74dc8c4bbfbc058aea4f619a992b
ASUS <nethmidileksha6@gmail.com>
2026-03-17 22:39:23 +0530

    create application.yml file and set port and another details update

--- api-gateway/src/main/java/com/spms/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/spms/api_gateway/ApiGatewayApplication.java
@@ -3,6 +3,7 @@ package com.spms.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
